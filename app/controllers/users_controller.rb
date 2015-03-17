94877cb55df1b72a9db97d1a7e01bdbf0a8cbc5b
Kenji Azami <k.azami0305@gmail.com>
2015-03-17 12:32:36 +0000

    Finish user index action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  before_filter :authenticate_user!, :except=>[:show]
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
   
   def show
     @user = User.find(params[:id])
