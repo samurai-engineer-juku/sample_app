f52db305ff12c3d98fb37e64155bf7e32365aa3a
Shinya Inoue <inosdevelopment@gmail.com>
2015-07-21 18:04:37 +0900

    modified rails3's before_filter to rails4's before_action

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,5 @@
 class MicropostsController < ApplicationController
-  before_filter :authenticate_user!
+  before_action :authenticate_user!
   before_action :correct_user,   only: :destroy
 
   def create
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :authenticate_user!, :except=>[:show]
+  before_action :authenticate_user!, :except=>[:show]
   before_action :admin_user,     only: :destroy
 
   def index
